cc3da2c04a65baa42fe0701236fb3c628c091916
maj3stiic <90041668+maj3stiic@users.noreply.github.com>
2022-02-02 16:55:59 +0200

    Update exercise3fname.php

--- exercise3fname.php
+++ exercise3fname.php
@@ -1,9 +1,54 @@
 <?php
 
+echo "<h3>Q1</h3>";
+$d = date("F");
+if ($d == "August"){
+    echo "It's August, so it's still holiday. <br>";
+    
+}
+else {
+    echo "Not August, This is  $d  so i don't have any holidays <br>";
+}
 
-echo "Hello there";
-echo"skkktrrtrtrtr";
-test etste wejhdfvsfnsdfls
+echo "<h3>Q2</h3>";
+$color = "Red";
+if ($color == "Red"){
+    echo "The color is red. <br>";
+    
+}
+else {
+    echo "The color is not red. <br>";
+}
+
+echo "<h3>Q3</h3>";
+
+
+
+
+$grade = 70;
+if ($grade >= 80){
+    echo "Excellent ! <br>";
+  
+}
+elseif ($grade >= 70){
+    echo "Great ! <br>";
+}
+elseif ($grade >= 60){
+    echo "Good !<br>";
+}
+elseif ($grade >= 50){
+    echo "Pass <br>";
+}
+else {
+    echo "Fail <br>";
+}
+
+echo "<h3>Q4</h3>";
+
+$age = 18;
+ if ($age == 18){
+     echo ""
+ }
 
 
 
